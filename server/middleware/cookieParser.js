ba8170434af958b9adc0cbfafdfb969d26eb3ac8
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-06-24 16:35:55 -0700

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,14 @@
 const parseCookies = (req, res, next) => {
+  let cookies = {};
+  if (req.headers.cookie) {
+    let cookieArr = req.headers.cookie.split(';');
+    cookieArr.forEach((cookieString) => {
+      let tempArr = cookieString.split('=');
+      cookies[tempArr[0].trim()] = tempArr[1];
+    });
+    req.cookies = cookies;
+  }
+  next();
 };
 
 module.exports = parseCookies;
